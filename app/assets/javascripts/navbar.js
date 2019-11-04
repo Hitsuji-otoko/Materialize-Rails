825a374b3e9d42d9bf21eac9947333ceda03b7f9
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-04 15:23:20 +0900

    apply-ImageCard

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
+    $(".sidenav").sidenav();
+    /* sidenav上でDropdownメニューを動かしたいが...
+    $('.sidenav').sidenav('show');
+    */
 });
