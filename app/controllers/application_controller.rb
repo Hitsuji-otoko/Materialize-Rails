e7e2370ea13460c41b9c719ed02d2d0401523a42
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-06 00:08:47 +0900

    ログイン時: emailプロパティも必要

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,18 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :user_name, :password, :password_confirmation ]
+    added_attrs = [ :email, :username, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+  
+  def after_sign_in_path_for(resource)
+    boards_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
