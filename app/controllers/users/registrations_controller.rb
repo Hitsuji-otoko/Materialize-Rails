0d22c0fd1fa758e194b875535ad13ca86e59ec77
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-06 20:07:50 +0900

    Login中にもユーザ登録を可能にする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: [:cancel]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
