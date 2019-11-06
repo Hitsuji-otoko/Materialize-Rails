77b99fd765d44c548d2faeddab97d23b4b35874c
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-06 21:29:50 +0900

    header: 新規投稿ボタン

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,23 @@
 class BoardsController < ApplicationController
-
+  before_action :authenticate_user!  
+  before_action :set_board, only: [:show, :edit, :update, :destroy]
   def index
-    @boards = Board.all
+    @boards = current_user.boards
+    binding.pry
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_board
+    # @note = Note.find(params[:id])
+    @board = current_user.boards.find_by(id: params[:id])
+    redirect_to boards_url if @board.nil?
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def board_params
+    params.require(:board).permit(:name, :title, :body, :user_id)
   end
 
 end
