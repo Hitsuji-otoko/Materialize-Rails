cd613dba885cb23d6eb027075a33fec2a5fb8d68
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-07 20:22:15 +0900

    Cardプレビューの実装

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $(".sidenav").sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
     /* sidenav上でDropdownメニューを動かしたいが...
     $('.sidenav').sidenav('show');
     */
